0326cd55bd368fd1b28e3483a519509c103fa474
Facundo Aletto <facundoaletto95@gmail.com>
2023-03-26 21:02:44 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+document.querySelector("body").innerHTML = `
+<h1>Sorry, page not found</h1>
+<img src="https://miro.medium.com/v2/resize:fit:640/1*82D2cg8Gpe9CVISaph6RPg.gif"
+        alt="Google Dino">
+`;
